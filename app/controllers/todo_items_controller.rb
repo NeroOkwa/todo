6a9f88467f7b2ead329b0936eebfabe617375f90
Nero Okwa <nero.okwa@hotmail.co.uk>
2020-04-18 00:23:51 -0700

    added ability to add todo list, add todo items to lists, and delete todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,11 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
 
-
-
-#def index
-#end
-
 def create
 	@todo_item = @todo_list.todo_items.create(todo_item_params)
 	redirect_to @todo_list
@@ -16,15 +11,16 @@ def destroy
 	if @todo_item.destroy
 		flash[:success] = "Todo List item was deleted"
 	else
-		flash[:error] = "Tdo List item could not be deleted."
+		flash[:error] = "Todo List item could not be deleted."
 	end
+	redirect_to @todo_list
 
 end
 
 private
 
 def set_todo_list
-	@todo_;ost = TodoList.find(params[:todo_list_id])
+	@todo_list = TodoList.find(params[:todo_list_id])
 end
 
 def todo_item_params
